fix bug of translation estimation and add feat of static/moving part var calculation

embodied_analogy/reasoning/belief_propagate.py
@@ -20,7 +20,7 @@ import sklearn.cluster as cluster
 from cotracker.utils.visualizer import Visualizer
 from embodied_analogy.pipeline.process_record import RecordDataReader
 from embodied_analogy.perception.online_cotracker import track_any_points
-from embodied_analogy.visualization.vis_tracks_3d import vis_tracks_3d_napari
+from embodied_analogy.visualization.vis_tracks_3d import vis_tracks_3d_napari, vis_pointcloud_series_napari
 from embodied_analogy.utility.utils import (
     depth_image_to_pointcloud, 
     visualize_pc, 
@@ -168,7 +168,7 @@ if visualize:
 
 # 1) 对初始帧中的物体点云聚类得到 num_clusters 个点簇, 并计算 num_clusters 个簇中心在图像上的投影
 # TODO: 可以在一开始聚类的时候也考虑视觉特征
-num_clusters = 300
+num_clusters = 600
 feat_for_kmeans = np.concatenate([pc_0, rgb_0], axis=-1)
 centroids, labels, _ = cluster.k_means(feat_for_kmeans, init="k-means++", n_clusters=num_clusters)
 
@@ -219,22 +219,34 @@ pred_tracks_3d = pred_tracks_3d.reshape(T, M, 3) # T, M, 3
 #     vis_tracks_3d_napari(pred_tracks_3d)
     
 motion_for_kmeans = pred_tracks_3d.permute(1, 0, 2).reshape(M, -1) # M T*3
-
 _, moving_labels, _ = cluster.k_means(motion_for_kmeans.cpu().numpy(), init="k-means++", n_clusters=2)
-red_and_green = np.array([[1, 0, 0], [0, 1, 0]])
-rigid_part_colors = red_and_green[moving_labels] # M, 3
+
+# 在这里将分类后的两类点进行判断, 判断哪一类是静止的, 哪一类是运动的 (M = M1 + M2)
+part1 = pred_tracks_3d[:, moving_labels == 0, :] # T, M1, 3
+part2 = pred_tracks_3d[:, moving_labels == 1, :] # T, M2, 3
+
+# 计算点的轨迹的 variance, 将 variance 小的点认为是静止的, variance 大的点认为是运动的
+from embodied_analogy.utility.utils import tracks3d_variance
+if tracks3d_variance(part1) > tracks3d_variance(part2):
+    moving_mask = (moving_labels == 0)
+    static_mask = (moving_labels == 1)
+else:
+    moving_mask = (moving_labels == 1)
+    static_mask = (moving_labels == 0)
 
 if napari:
+    red_and_green = np.array([[1, 0, 0], [0, 1, 0]])
+    rigid_part_colors = red_and_green[moving_labels] # M, 3
     vis_tracks_3d_napari(pred_tracks_3d, rigid_part_colors)
     
-if visualize:
-    visualize_pc(pred_tracks_3d[:M, :].cpu().numpy(), rigid_part_colors)
+# if visualize:
+#     visualize_pc(pred_tracks_3d[:M, :].cpu().numpy(), rigid_part_colors)
     
 # 在这里跑 sam2, 得到 moving_part 和 static_part 的分割
 from embodied_analogy.perception.sam2_masking import run_sam2_whole
 from embodied_analogy.visualization.vis_sam2_mask import visualize_sam2_mask
-moving_tracks_2d = pred_tracks_2d[:, moving_labels == 1, :] # T, N, 2
-static_tracks_2d = pred_tracks_2d[:, moving_labels == 0, :] # T, N, 2
+moving_tracks_2d = pred_tracks_2d[:, moving_mask, :] # T, N, 2
+static_tracks_2d = pred_tracks_2d[:, static_mask, :] # T, N, 2
 point_prompt = torch.stack([moving_tracks_2d[0][0], static_tracks_2d[0][0]]).cpu() #2, 2
 video_masks = run_sam2_whole(rgb_folder, initial_point_prompt=point_prompt)
 if visualize:
@@ -249,10 +261,28 @@ if save_intermidiate:
 # 5) 初步估计出 joint parameters
 from embodied_analogy.estimation.joint_estimate_w_corr import estimate_translation_from_tracks
     
-translation_c, scales = estimate_translation_from_tracks(pred_tracks_3d)
+translation_c, scales = estimate_translation_from_tracks(pred_tracks_3d[:, moving_mask, :])
 Rc2w = Tw2c[:3, :3].T # 3, 3
 translation_w = Rc2w @ translation_c
 
+# for debug, visualize 原始的跟踪点，和第一frame的moving points 按照我们的估计的joint param 计算出的轨迹
+if visualize:
+    tracks_3d = pred_tracks_3d.cpu().numpy() # T, M, 3
+    moving_points = tracks_3d[0, moving_mask, :] # M, 3
+    # static_points = tracks_3d[0, moving_labels == 0, :]
+    points = []
+    colors = []
+    for i in range(len(tracks_3d)):
+        points_tmp = []
+        colors_tmp = []
+        points_tmp.extend(tracks_3d[i])
+        colors_tmp.extend([[0, 0, 1]] * len(tracks_3d[i]))
+        points_tmp.extend(moving_points + scales[i] * translation_c)
+        colors_tmp.extend([[1, 0, 0]] * len(moving_points))
+        points.append(np.array(points_tmp))
+        colors.append(np.array(colors_tmp))
+        
+    vis_pointcloud_series_napari(points, colors)
 # 将 joint states 保存到 tmp_folder
 if save_intermidiate:
     np.savez(

embodied_analogy/reasoning/frame_to_frame.py
@@ -65,28 +65,37 @@ def classify_mask(K, depth_ref, depth_tgt, obj_mask_ref, obj_mask_tgt, T_ref2tgt
     uv_static_pred_int = np.floor(uv_static_pred).astype(int)
     mask_static_obs = obj_mask_tgt[uv_static_pred_int[:, 1], uv_static_pred_int[:, 0]] # N
     depth_static_obs = depth_tgt[uv_static_pred_int[:, 1], uv_static_pred_int[:, 0]] # N
-    # static_score = alpha * (mask_static_obs - 1)
+    
+    alpha = 0.
     static_score = alpha * (mask_static_obs - 1) + np.minimum(0, depth_static_pred - depth_static_obs) # N
+    # static_score = alpha * (mask_static_obs - 1) + (depth_static_pred - depth_static_obs) # N
     
     uv_moving_pred_int = np.floor(uv_moving_pred).astype(int)
     mask_moving_obs = obj_mask_tgt[uv_moving_pred_int[:, 1], uv_moving_pred_int[:, 0]]
     depth_moving_obs = depth_tgt[uv_moving_pred_int[:, 1], uv_moving_pred_int[:, 0]]
-    # moving_score = alpha * (mask_moving_obs - 1)
     moving_score = alpha * (mask_moving_obs - 1) + np.minimum(0, depth_moving_pred - depth_moving_obs) # N
+    # moving_score = alpha * (mask_moving_obs - 1) + (depth_moving_pred - depth_moving_obs) # N
     
     # 1.5）根据 static_score 和 moving_score 将所有点分类为 static, moving 和 unknown 中的一类
     # 得分最大是 0, 如果一方接近 0，另一方很小，则选取接近为 0 的那一类， 否则为 unkonwn
     class_mask = np.zeros(len(static_score))
     for i in range(len(static_score)):
         if depth_static_obs[i] == 1e6 or depth_moving_obs[i] == 1e6:
-            # class_mask[i] = 2 # 2 for unknown
-            class_mask[i] = 0
+            class_mask[i] = 0 # 2 for unknown
             continue
-        if abs(static_score[i]) <= abs(moving_score[i]) :
-            class_mask[i] = 0 # 0 for static
+        
+        if abs(static_score[i]) > 1 and abs(moving_score[i]) > 1:
+            class_mask[i] = 0 
+            continue
+        
+        if abs(static_score[i]) < 0.1 and abs(moving_score[i]) < 0.1:
+            class_mask[i] = 0 
+            continue
+        
+        if static_score[i] + 0.001 < moving_score[i]:
+            class_mask[i] = 1 # 0 for static
         else:
-            print(static_score[i], moving_score[i])
-            class_mask[i] = 1 # 1 for moving
+            class_mask[i] = 0 
     return class_mask.astype(np.bool_)
 def refine_joint_est(depth1, depth2, obj_mask1, obj_mask2, joint_param, delta_joint_state):
     """
@@ -101,23 +110,31 @@ if __name__ == "__main__":
     
     translation_w = joint_state_npz["translation_w"]
     translation_c = joint_state_npz["translation_c"]
+    translation_w_gt = np.array([0, 0, 1.])
+    
+    Rc2w = np.array([[-4.83808517e-01, -1.60986036e-01,  8.60239983e-01],
+       [-8.75173867e-01,  8.89947787e-02, -4.75552976e-01],
+       [ 5.21540642e-07, -9.82936144e-01, -1.83947206e-01]])
+    translation_c_gt = Rc2w.T @ translation_w_gt
+    
+    ref_idx, tgt_idx = 0, 38
     
     # 读取 0 和 47 帧的深度图
-    depth_ref = np.load(os.path.join(tmp_folder, "depths", "0.npy")).squeeze() # H, w
-    depth_tgt = np.load(os.path.join(tmp_folder, "depths", "47.npy")).squeeze()
+    depth_ref = np.load(os.path.join(tmp_folder, "depths", f"{ref_idx}.npy")).squeeze() # H, w
+    depth_tgt = np.load(os.path.join(tmp_folder, "depths", f"{tgt_idx}.npy")).squeeze()
     
     # 修改深度图观测为 0 的地方
     depth_ref[depth_ref == 0] = 1e6
     depth_tgt[depth_tgt == 0] = 1e6
     
     # 读取 0 和 47 帧的 obj_mask
-    obj_mask_ref = np.array(Image.open(os.path.join(tmp_folder, "masks", "0.png"))) # H, W 0, 255
-    obj_mask_tgt = np.array(Image.open(os.path.join(tmp_folder, "masks", "47.png")))
+    obj_mask_ref = np.array(Image.open(os.path.join(tmp_folder, "masks", f"{ref_idx}.png"))) # H, W 0, 255
+    obj_mask_tgt = np.array(Image.open(os.path.join(tmp_folder, "masks", f"{tgt_idx}.png")))
     
     obj_mask_ref = (obj_mask_ref == 255)
     obj_mask_tgt = (obj_mask_tgt == 255)
     
-    delta_scale = scales[47] - scales[0]
+    delta_scale = scales[tgt_idx] - scales[ref_idx]
     T_ref_to_tgt = np.eye(4)
     # from 0 to 47, which means coor_47 = coor_0 + translation_c * delta_scale
     T_ref_to_tgt[:3, 3] = translation_c * delta_scale
@@ -127,11 +144,27 @@ if __name__ == "__main__":
         [  0., 300., 300.],
         [  0.,   0.,   1.]]
     )
+    # 可视化 tgt frame 的点云, 和 ref frame 的点云经过 transform 后与之的对比，看看估计的到底有多离谱，你妈的
+    from embodied_analogy.utility.utils import visualize_pc
+    points_ref = depth_image_to_pointcloud(depth_ref, obj_mask_ref, K) # N, 3
+    points_ref_transformed = points_ref + translation_c * delta_scale 
+    colors_ref = np.zeros((len(points_ref), 3))
+    colors_ref[:, 0] = 1
+    # visualize_pc(points=points_ref, colors=None)
     
-    class_mask_ref = classify_mask(K, depth_ref, depth_tgt, obj_mask_ref, obj_mask_tgt, T_ref_to_tgt, alpha=1.)
-    recon_mask_ref = reconstruct_mask(obj_mask_ref, class_mask_ref)
-    Image.fromarray((recon_mask_ref.astype(np.int32) * 255).astype(np.uint8)).save("mask_ref.png")
+    points_tgt = depth_image_to_pointcloud(depth_tgt, obj_mask_tgt, K)
+    colors_tgt = np.zeros((len(points_tgt), 3))
+    colors_tgt[:, 1] = 1
     
-    class_mask_tgt = classify_mask(K, depth_tgt, depth_ref, obj_mask_tgt, obj_mask_ref, np.linalg.inv(T_ref_to_tgt), alpha=1.)
-    recon_mask_tgt = reconstruct_mask(obj_mask_tgt, class_mask_tgt)
-    Image.fromarray((recon_mask_tgt.astype(np.int32) * 255).astype(np.uint8)).save("mask_tgt.png")
+    points_concat_for_vis = np.concatenate([points_ref_transformed, points_tgt], axis=0)
+    colors_concat_for_vis = np.concatenate([colors_ref, colors_tgt], axis=0)
+    visualize_pc(points=points_concat_for_vis, colors=colors_concat_for_vis)
+    # visualize_pc(points=points_tgt, colors=colors_tgt)
+    if True:
+        class_mask_ref = classify_mask(K, depth_ref, depth_tgt, obj_mask_ref, obj_mask_tgt, T_ref_to_tgt, alpha=1.)
+        recon_mask_ref = reconstruct_mask(obj_mask_ref, class_mask_ref)
+        Image.fromarray((recon_mask_ref.astype(np.int32) * 255).astype(np.uint8)).save("mask_ref.png")
+        
+        class_mask_tgt = classify_mask(K, depth_tgt, depth_ref, obj_mask_tgt, obj_mask_ref, np.linalg.inv(T_ref_to_tgt), alpha=1.)
+        recon_mask_tgt = reconstruct_mask(obj_mask_tgt, class_mask_tgt)
+        Image.fromarray((recon_mask_tgt.astype(np.int32) * 255).astype(np.uint8)).save("mask_tgt.png")

embodied_analogy/utility/utils.py
@@ -281,7 +281,8 @@ def depth_image_to_pointcloud(depth_image, mask, K):
     - pointcloud (np.array): 点云，大小为 (N, 3)，表示每个像素点在相机坐标系下的三维坐标
     """
     assert depth_image.ndim == 2, "depth_image must be a 2D array"
-    assert mask.dtype == np.bool_, "mask must be a boolean array"
+    if mask is not None:
+        assert mask.dtype == np.bool_, "mask must be a boolean array"
     # 获取相机内参
     f_x = K[0, 0]  # 水平焦距 (单位: 像素)
     f_y = K[1, 1]  # 垂直焦距 (单位: 像素)
@@ -546,6 +547,28 @@ def reconstruct_mask(mask1: np.ndarray, mask2: np.ndarray) -> np.ndarray:
     new_mask[selected_indices[:, 0], selected_indices[:, 1]] = True
     
     return new_mask
+
+def tracks3d_variance(tracks: torch.Tensor) -> torch.Tensor:
+    """
+    计算 T, M, 3 形状的 3D 轨迹数据的平均方差 (PyTorch 版本)。
+
+    参数:
+        tracks (torch.Tensor): 形状为 (T, M, 3) 的张量，表示 T 个时刻 M 个点的 3D 轨迹。
+
+    返回:
+        torch.Tensor: M 个点的方差均值（标量）。
+    """
+    # 计算每个点在 T 个时刻的方差 (M, 3)
+    pointwise_variance = torch.var(tracks, dim=0, unbiased=False)  # 计算 M 个点的 3 维方差
+    
+    # 计算每个点的总方差（对 3 维坐标求均值）
+    pointwise_variance_mean = torch.mean(pointwise_variance, dim=1)  # (M,)
+
+    # 计算所有点的方差均值
+    average_variance = torch.mean(pointwise_variance_mean)
+
+    return average_variance
+
 
 if __name__ == "__main__":
     H, W = 300, 400

embodied_analogy/visualization/vis_tracks_3d.py
@@ -5,26 +5,52 @@ import torch
 import numpy as np
 import napari
 
-def vis_tracks_3d_napari(pc_series, colors):
+def vis_tracks_3d_napari(tracks_3d, colors):
     """
     Args:
-        pc_series: np.array of shape (T, M, 3)
+        tracks_3d: np.array of shape (T, M, 3)
         colors: np.array of shape (M, 3)
     """
-    # 首先将输入的 pc_series 转换为 napari 支持的格式, 即 T * M, (1t+3d) 的形式
-    if isinstance(pc_series, torch.Tensor):
-        pc_series = pc_series.cpu().numpy()
+    # 首先将输入的 tracks_3d 转换为 napari 支持的格式, 即 T * M, (1t+3d) 的形式
+    if isinstance(tracks_3d, torch.Tensor):
+        tracks_3d = tracks_3d.cpu().numpy()
         
-    T, M, _ = pc_series.shape
+    T, M, _ = tracks_3d.shape
     napari_data = np.zeros((T * M, 1 + 3))
     for i in range(T):
         napari_data[i * M: (i + 1) * M, 0] = i
-        napari_data[i * M: (i + 1) * M, 1:] = pc_series[i]
+        napari_data[i * M: (i + 1) * M, 1:] = tracks_3d[i]
     
     viewer = napari.Viewer(ndisplay=3)
     # 将 M, 3 大小的 colors 变换为 T*M, 3 的大小
     colors = np.tile(colors, (T, 1))
     viewer.add_points(napari_data, size=0.02, name='tracks_3d', opacity=0.8, face_color=colors)
+    # viewer.add_labels(np.random.randint(0, 10, size=(M, 1)), name='labels')
+    napari.run()
+    
+def vis_pointcloud_series_napari(pc_series, colors):
+    """
+    Args:
+        pc_series: list of np.arrays of shape (M, 3)
+        colors: list of np.arrays of shape (M, 3)
+    """
+    # 首先将输入的 pc_series 转换为 napari 支持的格式, 即 T * M, (1t+3d) 的形式
+    # if isinstance(pc_series, torch.Tensor):
+    #     pc_series = pc_series.cpu().numpy()
+        
+    T = len(pc_series)
+    napari_data = []
+    for i in range(T):
+        pc = pc_series[i] # M, 3
+        pc_with_t = np.concatenate([np.ones((pc.shape[0], 1)) * i, pc], axis=1) # M, 4
+        napari_data.append(pc_with_t)
+    napari_data = np.concatenate(napari_data, axis=0)
+    
+    viewer = napari.Viewer(ndisplay=3)
+    # 将 M, 3 大小的 colors 变换为 T*M, 3 的大小
+    if colors is not None:
+        colors = np.concatenate(colors, axis=0)
+    viewer.add_points(napari_data, size=0.02, name='pc_series', opacity=0.8, face_color=colors)
     # viewer.add_labels(np.random.randint(0, 10, size=(M, 1)), name='labels')
     napari.run()
 