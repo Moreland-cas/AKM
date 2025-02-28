cc948fde6ccf6c1fa781e8ace4e0991b4611a244
zhangboyuan <zhangboyuan@bigai.ai>
2025-02-28 17:28:46 +0800

    change create env.sh

--- create_env.sh
+++ create_env.sh
@@ -52,28 +52,29 @@ cd third_party
 # git clone git@github.com:NVIDIA/MinkowskiEngine.git
 cd MinkowskiEngine
 python setup.py install --blas_include_dirs=${CONDA_PREFIX}/include --blas=openblas
+cd ../
 
 # 安装 graspnetAPI
 echo "Installing graspnetAPI..."
-cd ../
 # git clone git@github.com:graspnet/graspnetAPI.git
 cd graspnetAPI
 pip install -e .
+cd ..
 
 # 安装 pointnet2
-cd ../pointnet2
+cd pointnet2
 python setup.py install
+cd ../
 
 # 安装 FeatUp
 echo "Installing FeatUp..."
-cd ../
 # git clone https://github.com/mhamilton723/FeatUp.git
 cd FeatUp
 pip install -e .
+cd ../
 
 # 安装 DIFT
-echo "Installing DIFT..."
-cd ../
+echo "Installing DIFT related support..."
 pip3 install -U xformers --index-url https://download.pytorch.org/whl/cu121
 pip install jupyterlab ipympl triton transformers matplotlib diffusers==0.15.0 accelerate
 pip install huggingface_hub==0.26 -i https://pypi.tuna.tsinghua.edu.cn/simple
@@ -84,24 +85,25 @@ echo "Installing co-tracker..."
 cd co-tracker
 pip install -e .
 # pip install matplotlib flow_vis tqdm tensorboard 'imageio[ffmpeg]'
+cd ../
 
 # 安装 SAM 2
 echo "Installing SAM 2..."
-cd ../
 # git clone https://github.com/facebookresearch/sam2.git 
 cd sam2
 pip install -e ".[notebooks]"
+cd ../
 
 # 安装 Grounding DINO
 echo "Installing Grounding DINO..."
-cd ../
 # git clone git@github.com:IDEA-Research/GroundingDINO.git
 cd GroundingDINO
 pip install -e .
+cd ..
 
 # 安装 Embodied Analogy
 echo "Installing Embodied Analogy..."
-cd ../../
+cd ../
 pip install -e .
 
 # 修复 opencv-python qt5 bug
@@ -115,6 +117,10 @@ python -m pip install "napari[pyqt5]" numpy==1.24.4
 
 # 删除 opencv 中的 qt5 插件, 防止与 cv2 冲突
 # rm /home/zby/ProgramFiles/anaconda3/envs/$ENV_NAME/lib/python3.10/site-packages/cv2/qt/plugins/platforms/libqxcb.so
+
+# 安装 RAM 相关支持
+echo "Installing RAM..."
+pip install open_clip_torch einops openai
 
 echo "Setup complete!"
 
