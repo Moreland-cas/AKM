3b4bee3e6e7fc2435b7e415f5d15e2380f5c5f37
zhangboyuan <zhangboyuan@bigai.ai>
2025-03-14 17:52:03 +0800

    add support to augment the input direction of anygrasp point cloud

--- embodied_analogy/grasping/anygrasp.py
+++ embodied_analogy/grasping/anygrasp.py
@@ -2,6 +2,7 @@ import numpy as np
 import argparse
 import open3d as o3d
 from scipy.spatial.transform import Rotation as R
+from graspnetAPI.grasp import GraspGroup
 from embodied_analogy.utility.utils import (
     visualize_pc,
     world_to_image,
@@ -60,6 +61,83 @@ def detect_grasp_anygrasp(points, colors, dir_out, visualize=False):
     if visualize:
         visualize_pc(points, colors, gg)
     return gg
+
+def detect_grasp_anygrasp(points, colors, dir_out, augment=True, visualize=False):
+    '''
+    输入世界坐标系下的点云和颜色, 返回 grasp_group (存储了信息 Tgrasp2w)
+        函数输入的 points 是世界坐标系下的
+        网络输入的点是 app 坐标系下的, app 坐标系需要根据 dir_out 来确定 (app 的 z 轴指向 dir_out 的反方向)
+        dir_out: 需要在 points 的坐标系(一般是世界坐标系)下指定, 从物体内部指向物体外部
+        网络的输出 grasp 的坐标系由 graspnetAPI 定义, grasp 存储了信息 Tgrasp2app
+        
+        可视化是在 world 坐标系下的
+        
+        NOTE: augment 为 True 时, 会对 dir_out 进行多个绕动, 分别预测 grasp, 然后将不同 grasp 在合并到一个坐标系下
+    '''
+    # load model
+    from gsnet import AnyGrasp # gsnet.so
+    # get a argument namespace
+    cfgs = argparse.Namespace()
+    cfgs.checkpoint_path = '/home/zby/Programs/Embodied_Analogy/assets/ckpts/checkpoint_detection.tar'
+    cfgs.max_gripper_width = 0.04
+    cfgs.gripper_height = 0.03
+    cfgs.top_down_grasp = False
+    cfgs.debug = visualize
+    model = AnyGrasp(cfgs)
+    model.load_net()
+    
+    
+    dir_out = dir_out / np.linalg.norm(dir_out)
+    # 接下来生成多个 dir_out
+    if augment:
+        random_perturb = np.random.randn(20, 3) 
+        dir_outs = dir_out + random_perturb * 0.5 # N, 3
+        dir_outs = dir_outs / np.linalg.norm(dir_outs, axis=1, keepdims=True)
+    else:
+        dir_outs = [dir_out]
+    
+    # 改变输入的类型
+    points = points.astype(np.float32)
+    colors = colors.astype(np.float32)
+    lims = np.array([-1, 1, -1, 1, -1, 1]) * 10
+    
+    ggs = GraspGroup()
+    for dir_out_i in dir_outs:
+        # 用不同的 dir_out 确定不同的 app 坐标系, coor_app = Rw2app @ coor_w, 也即 (0, 0, 1) = Rw2app @ -dir_out
+        Rw2app = rotation_matrix_between_vectors(-dir_out_i, np.array([0, 0, 1]))
+        points_input = points @ Rw2app.T # N, 3
+        points_input = points_input.astype(np.float32)
+        
+        # Tgrasp2app
+        gg, _ = model.get_grasp(
+            points_input,
+            colors, 
+            lims,
+            apply_object_mask=True,
+            dense_grasp=False,
+            collision_detection=True
+        )
+        # print('grasp num:', len(gg))
+        if gg == None:
+            continue
+        
+        # Tgrasp2w
+        zero_translation = np.array([[0], [0], [0]])
+        Rapp2w = Rw2app.T
+        Tapp2w = np.hstack((Rapp2w, zero_translation))
+        gg.transform(Tapp2w)
+        ggs.add(gg)
+    
+    if visualize:
+        visualize_pc(
+            points=points,
+            colors=colors,
+            grasp=ggs,
+            contact_point=np.array([0, 0, 0]),
+            post_contact_dirs=dir_outs
+        )
+        
+    return ggs
 
 def find_nearest_grasp(grasp_group, contact_point):
     '''
@@ -152,9 +230,21 @@ if __name__ == '__main__':
     points_world = points_camera @ Rc2w.T
     
     # visualize_pc(points_world, colors)
+    import time
+    start_time = time.time()
     detect_grasp_anygrasp(
         points=points_world, 
         colors=colors,
         dir_out=np.array([-1, 0, 0]),
-        visualize=True
+        augment=True,
+        visualize=False
     )
+    end_time = time.time()
+    print("time used:", end_time - start_time)
+    
+    # augment = False, consume 0.734
+    # augment = True, consume 1.79
+    # x + y = 0.73 x + 15y = 1.79(15) -> 2.5(20)
+    # y = 0.075
+    # x = 0.657
+    
